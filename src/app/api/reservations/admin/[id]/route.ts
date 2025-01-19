cf599fa7e6d2dac7dfad9fb6aef1ed24369165ec
b.eww_ <148910513+BernardoPer19@users.noreply.github.com>
2025-01-19 05:47:36 -1100

    Update route.ts

--- src/app/api/reservations/admin/[id]/route.ts
+++ src/app/api/reservations/admin/[id]/route.ts
@@ -5,43 +5,6 @@ export interface Params {
   params: { id: string };
 }
 
-// const GET = async ({params}: Params) => {
-//     const ReservationByID = prisma.reserva.findFirst()
-// }
-
-// export const DELETE = async (req: Request, { params }: Params) => {
-//     try {
-//       const { id } = params;  // Aquí extraemos el ID de los params
-      
-//       if (!id || isNaN(Number(id))) {
-//         return NextResponse.json(
-//           { error: "El ID de la reserva debe ser un número válido." },
-//           { status: 400 }
-//         );
-//       }
-  
-//       const deleteReservation = await prisma.reserva.delete({
-//         where: { id: Number(id) },
-//       });
-  
-//       return NextResponse.json(deleteReservation);
-  
-//     } catch (error: any) {
-//       console.error("Error al eliminar la reserva:", error);
-      
-//       if (error.code === "P2025") {
-//         return NextResponse.json(
-//           { error: "La reserva con el ID proporcionado no existe." },
-//           { status: 404 }
-//         );
-//       }
-  
-//       return NextResponse.json(
-//         { error: `Error al eliminar la reserva: ${error.message}` },
-//         { status: 500 }
-//       );
-//     }
-//   };
   
 export const PUT = async (request: Request, { params }: Params) => {
   try {
